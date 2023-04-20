2fec37fe243da61ecae32051f8e63dd33a0460d8
kameleonus <kamil10.00@o2.pl>
2023-04-20 15:05:12 +0200

    Start projektu kotlin kurs

--- src/main/java/com/example/learn/MainActivity.kt
+++ src/main/java/com/example/learn/MainActivity.kt
@@ -2,11 +2,31 @@ package com.example.learn
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.learn.model.FoodIngredient
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+
+    val ingredientArray by lazy{
+        val jsonStream = assets.open("base.json")
+        val size=jsonStream.available()
+        val buffer = ByteArray(size)
+        jsonStream.read(buffer)
+        jsonStream.close()
+        val jsonString = String(buffer,Charsets.UTF_8)
+
+        Gson().fromJson<List<FoodIngredient>>(jsonString, FOOD_ING_TYPE)
+    }
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+    companion object{
+        val FOOD_ING_TYPE = object: TypeToken<List<FoodIngredient>>(){}.type
     }
 }
--- src/main/java/com/example/learn/model/FoodIngredient.kt
+++ src/main/java/com/example/learn/model/FoodIngredient.kt
@@ -1,5 +1,7 @@
 package com.example.learn.model
 
+import android.graphics.drawable.Drawable
+import androidx.core.content.res.ResourcesCompat
 import com.example.learn.App
 import com.example.learn.R
 
@@ -18,29 +20,33 @@ data class FoodIngredient(
    private val imageName:String)
 {
     val intake
-        get() = """${"\u2642"} $dailyMale ${unit.getName()}
-            |${"\u2640"} $dailyFem ${unit.getName()}""".trimMargin()
-
+        get() = """${"\u2642"} $dailyMale ${unit.getLocalName()}
+            |${"\u2640"} $dailyFem ${unit.getLocalName()}""".trimMargin()
+    val drawable:Drawable?
+        get(){
+            val appContext = App.appContext
+           val id= appContext.resources.getIdentifier(imageName,DRAWABLE_TYPE_DEF,appContext.packageName)
+            return ResourcesCompat.getDrawable(appContext.resources,id,null)
+        }
+    companion object{
+        const val DRAWABLE_TYPE_DEF= "drawable"
+    }
 }
 
 enum class IntakeDoseUnit (){
     GRAM() {
-        override fun getName() {
-            App.appContext.getString(R.string.gram)
-        }
+        override fun getLocalName() = App.appContext.getString(R.string.gram)
+
     }, MGRAM() {
-        override fun getName() {
-            App.appContext.getString(R.string.miligram)
-        }
+        override fun getLocalName() = App.appContext.getString(R.string.miligram)
+
     }, UGRAM() {
-        override fun getName() {
-            App.appContext.getString(R.string.mikrogram)
-        }
+        override fun getLocalName() = App.appContext.getString(R.string.mikrogram)
+
     }, IU() {
-        override fun getName() {
-            App.appContext.getString(R.string.unifiedunit)
-        }
+        override fun getLocalName()= App.appContext.getString(R.string.unifiedunit)
+
     };
 
-    abstract fun getName()
+    abstract fun getLocalName():String?
 }
